b4a6ba43dce99a627c549f8b3eddda6fb61a8058
Quan Truong <qmtruong92@gmail.com>
2018-04-04 18:56:41 -0700

    Tweaking from 2 seconds to .5 seconds

--- setburnrate.cpp
+++ setburnrate.cpp
@@ -6,14 +6,13 @@
 
 
 double total_mass;
-double ship_weight = 1000.0;
+double ship_weight = 534.0;
 double force_one;
 double max_velocity = 110.0;
 double burn_rate;
 double v_nozz = 2000.0;
 
 int main() {
-    std::cout << "Hello world\n";
     ship_state_type *rocket = new ship_state_type;
     rocket->fuel_weight = 1000.0;
     rocket->acceleration = 0.0; /* Initial acceleration = 0 */
@@ -31,20 +30,29 @@ int main() {
 
     while (rocket->time < 500) {
 
-        while (rocket->time < 200) {
+        while (rocket->time < 50) {
             total_mass = ship_weight + rocket->fuel_weight;
+            std::cout << "Total mass: " << total_mass << std::endl;
             force_one = total_mass * 110.0;
             burn_rate = force_one / v_nozz;
-            rocket->fuel_weight = burn_rate * delta_t;
-            std::cout << "Burn phase: \n";
+            rocket->fuel_weight = (rocket->fuel_weight) - burn_rate * delta_t;
+            total_mass = ship_weight + rocket->fuel_weight;
+            rocket->acceleration = force_one / total_mass; /* in m/s^2*/
+            rocket->altitude = rocket->velocity * 0.1 + 0.5 * rocket->acceleration * 0.1;
+            std::cout << "Rocket altitude: " << rocket->altitude << std::endl;
+            rocket->velocity = rocket->velocity + rocket->acceleration * 0.1;
+            std::cout << "Rocket final velocity: " << rocket->velocity << std::endl;
+            std::cout << "Rocket fuel weight: \n";
             std::cout << rocket->fuel_weight;
+            std::cout << "\nTICK: ";
+            std::cout << rocket->time;
             std::cout << "\n";
             // loop shit
             /*setBurnRate(*rocket);*/
             sleep((unsigned int)0.1);
             rocket->time += 1;
         }
-        std::cout << "Out of burn phase: \n";
+      /*  std::cout << "Out of burn phase: \n";*/
         setBurnRate(*rocket);
         sleep((unsigned int)0.1);
         rocket->time += 1;
